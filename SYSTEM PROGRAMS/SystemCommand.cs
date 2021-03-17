f5076fc53921bfaaa4bf5347e04aa8ad6e9b41c9
Apolyon <80668841+apolyon77@users.noreply.github.com>
2021-03-16 17:34:47 -0700

    1.0.0

---
+++ SYSTEM PROGRAMS/SystemCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+// x2 - by Commander Apolyon
+// Not for public distribution
+
+namespace x2
+{
+    public class SystemCommand : program
+    {
+        public void code()
+        {
+            Console.WriteLine("> SystemCommand online");
+        }
+    }
+}
---
+++ SYSTEM PROGRAMS/UserConsole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+
+// x2 - by Commander Apolyon
+// Not for public distribution
+
+namespace x2
+{
+    public class UserConsole : program
+    {
+        public void code()
+        {
+            // use apache2 at http://192.168.1.1            
+            Console.WriteLine("> UserConsonle online");
+        }
+    }
+}
