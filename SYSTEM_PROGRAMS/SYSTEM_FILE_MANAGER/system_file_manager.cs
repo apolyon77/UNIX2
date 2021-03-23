f5f9ada9cec728bd8e395a7f4283e4665c39a122
Apolyon <80668841+apolyon77@users.noreply.github.com>
2021-03-23 00:47:44 -0700

    Add files via upload

---
+++ SYSTEM_PROGRAMS/SYSTEM_FILE_MANAGER/system_file_manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+// x2 - by Commander Apolyon
+// Not for public distribution
+
+namespace x2
+{
+    public class system_file_manager : program
+    {
+        public void code(object x)
+        {
+            Console.WriteLine("> system_file_manager online");
+        }
+    }
+}
---
+++ SYSTEM_PROGRAMS/SYSTEM_MESSENGER/system_messenger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+// x2 - by Commander Apolyon
+// Not for public distribution
+
+namespace x2
+{
+    public class system_messenger : program
+    {
+        public void code(object x)
+        {
+            Console.WriteLine("> system_messenger online");
+        }
+    }
+}
